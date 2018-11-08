6910fa7286f513ec5711117f189f5bf759d63794
Justin Sedge <sedgejustin@gmail.com>
2018-11-07 22:01:12 -0400

    Add basic player movement and ship model

---
+++ Assets/Scipts/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	public string axis;
+	public float sensitivityX;
+	public float sensitivityY;
+	public bool useJoystick;
+	// Update is called once per frame
+	void Update () {
+		// Get the input from the Joystick or Mouse axis
+		float x,y;
+		if(!useJoystick){
+			x = Input.GetAxis("Mouse X") * sensitivityX;
+			y = -Input.GetAxis("Mouse Y") * sensitivityY;
+		}else{
+			x = Input.GetAxis("Joystick X") * sensitivityX;
+			y = Input.GetAxis("Joystick Y") * sensitivityY;
+		}
+		
+
+		// Choose which axis this is set to move around
+		if(axis == "X")
+			transform.Rotate(0,x,0);
+		else if(axis == "Y")
+			transform.Rotate(y,0,0);
+	}
+}
---
+++ Assets/Scipts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed = 1.0f;
+	private CharacterController controller;
+
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent(typeof(CharacterController)) as CharacterController;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDir = transform.TransformDirection(moveDir);
+		moveDir*=Time.deltaTime*speed;
+		var flags = controller.Move(moveDir);
+	}
+}
